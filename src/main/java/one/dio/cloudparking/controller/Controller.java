8f4f9a71ec8ca5e9ef8f9b080a3ea23ecd081b77
William Mello <mello.william01@gmail.com>
2022-02-11 09:17:10 -0300

    hello

---
+++ src/main/java/one/dio/cloudparking/controller/Controller.java
@@ -0,0 +1,13 @@
+package one.dio.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Controller {
+
+public class HelloController {
+
+    @GetMapping
+    public String hello() {return "Hello, Will!";}
+}
+
+}
--- src/main/java/one/dio/cloudparking/controller/HelloController.java
+++
@@ -1,7 +0,0 @@
-package one.dio.cloudparking.controller;
-
-public class HelloController {
-
-
-
-}
