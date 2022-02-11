b108d4822e47faa45d16ce552f6cf5009e21cc40
William Mello <mello.william01@gmail.com>
2022-02-11 09:20:30 -0300

    insert @rest

--- src/main/java/one/dio/cloudparking/controller/Controller.java
+++ src/main/java/one/dio/cloudparking/controller/Controller.java
@@ -1,13 +1,13 @@
 package one.dio.cloudparking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 public class Controller {
-
-public class HelloController {
 
     @GetMapping
     public String hello() {return "Hello, Will!";}
-}
-
 }
